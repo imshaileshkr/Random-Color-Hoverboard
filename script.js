6ce1f1c73a19f0e4f78148cb0cbe161eaa4a379f
imshaileshkr <imshaileshkr@users.noreply.github.com>
2021-02-02 16:44:05 +0530

    Update in getRandomColor function
    Update in getRandomColor function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container');
 
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22','#2ecc74'];
+// const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22','#2ecc74'];
 
 const SQUARES = 500;
 
@@ -14,22 +14,22 @@ for(let i = 0; i < SQUARES; i++ ) {
     container.appendChild(square);
 }
 
-        // set color
+// set color
 function setColor(element) {
     // console.log(element);
     const color = getRandomColor();
-    element.style.background = color;
-    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+    element.style.background = `#${color}`;
+    element.style.boxShadow = `0 0 2px #${color}, 0 0 10px #${color}`;
 }
 
-        // Remove color
+// Remove color
 function removeColor(element) {
     element.style.background = `#1d1d1d`;
     element.style.boxShadow = `0 0 2px #000`;
 }
 
-        // Get random color
+// Get random color
 function getRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)];
-   
+    // return colors[Math.floor(Math.random() * colors.length)];
+    return Math.floor(Math.random()*16777215).toString(16);
 }
